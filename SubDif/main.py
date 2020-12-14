fe16bdedea3f0719ed787795e6c40eaeb66b38c0
tvvoty <neketa777@gmail.com>
2020-12-14 07:52:43 +0300

    for a folder, doesnt work now somehow

--- SubDif/main.py
+++ SubDif/main.py
@@ -1,20 +1,62 @@
 from pysubparser import parser
 import freq_dic
 import MeCab
+import os
 
-
-#put there absolute path of the file with double slashes eg 'E:\PythonProjects\pythonProject1\Anime frec list\[Kamigami] Barakamon - 01 [1280×720 x264 AAC Sub(Chs,Jap)].ass'
-sub_file = 'put there path of the file'
-subtitles = parser.parse('E:\\PythonProjects\\pythonProject1\\Anime frec list\\Psycho-Pass_001.srt')
-
-#test, works
-# for subtitle in subtitles:
-#     print(subtitle.text)
-
-#parse subtitles with MeCab
+# parse subtitles with MeCab
 wakati = MeCab.Tagger("-Owakati")
 
 
+
+# dir with subtitles
+os.chdir("F:\GitHub\AvJDif\SubDif\subs\Barakamon")
+
+def find_avg_diff_of_folder():
+    sum_of_diff = 0
+    wordcount = 0
+    for filename in os.listdir(os.getcwd()):
+        filepath = (os.path.join(os.getcwd(), filename)
+        subtitles = parser.parse(filepath)
+
+        # for every line in the file
+        for subtitle in subtitles:
+            print(subtitle.text)
+            # print(subtitle.text.encode("utf-8"))
+            parsed_line = wakati.parse(subtitle.text).split()
+            print(parsed_line)
+            # print(parsed_line.encode("utf-8"))
+            for word in parsed_line:
+                print(word)
+                # print(word.encode("utf-8"))
+                for entry in freq_dic.freq_list:
+                    if word == entry[0]:
+                        wordcount += 1
+                        sum_of_diff += entry[2]
+                        print(entry[2])
+    print(sum_of_diff)
+    print(wordcount)
+    print(sum_of_diff / wordcount)
+
+find_avg_diff_of_folder()
+
+
+
+
+
+def dum():
+    # test
+    # for subtitle in subtitles:
+    #     print(subtitle.text)
+    pass
+
+
+
+
+
+
+
+
+# old function for one file
 def find_avg_diff():
     sum_of_diff = 0
     wordcount = 0
@@ -23,6 +65,7 @@ def find_avg_diff():
         print(subtitle.text)
         # print(subtitle.text.encode("utf-8"))
         parsed_line = wakati.parse(subtitle.text).split()
+        print(parsed_line)
         # print(parsed_line.encode("utf-8"))
         for word in parsed_line:
             print(word)
@@ -35,11 +78,3 @@ def find_avg_diff():
     print(sum_of_diff)
     print(wordcount)
     print(sum_of_diff / wordcount)
-
-
-                # else:
-                #     print("no")
-        #         print(shit.shitlist.index(word))
-
-
-find_avg_diff()
