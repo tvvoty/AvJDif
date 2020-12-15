22ce02137ff8f3c33986fcff08db0d9c44f925fa
tvvoty <75815245+tvvoty@users.noreply.github.com>
2020-12-15 07:52:14 +0300

    A file to translane freq list to a hash table and the converted hashtable

---
+++ SubDif/transform_freqdiclist_to_dichashtable.py
@@ -0,0 +1,9 @@
+import freq_dic
+
+freq_hashtable = {entry[0]: entry[2] for entry in freq_dic.freq_list}
+
+# for entry in freq_dic.freq_list:
+#     d = {entry[0] : entry[1:]}
+
+with open('freq_dic_hashtable.py', 'w', encoding='utf-8') as f:
+    f.write(f'freq_hashtable = {freq_hashtable}')
