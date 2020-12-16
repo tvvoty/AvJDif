a453c8e6b42e14dd6be775adc156203cbf3c27fc
tvvoty <75815245+tvvoty@users.noreply.github.com>
2020-12-16 12:58:26 +0300

    Working parsing of subfolders without zips

--- SubDif/main_globalwith_test.py
+++ SubDif/main_globalwith_test.py
@@ -4,13 +4,6 @@ import freq_dic_hashtable
 import MeCab
 import os
 
-# parse subtitles with MeCab
-wakati = MeCab.Tagger("-Owakati")
-
-main_folder = ("F:\\GitHub\\AvJDif\\SubDif\\sub_archive_simulation")
-with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.txt", mode='w', encoding='utf-8') as f:
-    f.write('anime_avg_diff_table = ')
-
 
 def find_avg_diff_of_folder_with_hashtable():
     for folder in os.listdir(main_folder):
@@ -40,43 +33,50 @@ def find_avg_diff_of_folder_with_hashtable():
         # print(sum_of_diff)
         # print(wordcount)
         print(sum_of_diff / wordcount)
-        with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.txt", mode='w', encoding='utf-8') as f:
-            f.write(f'[{folder},{sum_of_diff},{wordcount},{sum_of_diff / wordcount}], ')
-
-
-find_avg_diff_of_folder_with_hashtable()
+        f.write(f'[{folder},{sum_of_diff},{wordcount},{sum_of_diff / wordcount}], ')
 
 
 
+# parse subtitles with MeCab
+wakati = MeCab.Tagger("-Owakati")
+
+main_folder = ("F:\\GitHub\\AvJDif\\SubDif\\sub_archive_simulation")
+with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.txt", mode='w', encoding='utf-8') as f:
+    f.write('anime_avg_diff_table = ')
+
+    find_avg_diff_of_folder_with_hashtable()
 
 
 
 
 
-def find_avg_diff_of_folder():
-    sum_of_diff = 0
-    wordcount = 0
-    for file in os.listdir(os.getcwd()):
-        file_path = (os.path.join(os.getcwd(), file))
-        print(file_path)
-        subtitles = parser.parse(file_path)
-        print(subtitles)
 
 
-        for subtitle in subtitles:
-            print(subtitle.text)
-            parsed_line = wakati.parse(subtitle.text).split()
-            print(parsed_line)
-            for word in parsed_line:
-                print(word)
-                for entry in freq_dic.freq_list:
-                    if word == entry[0]:
-                        wordcount += 1
-                        sum_of_diff += entry[2]
-                        print(entry[2])
-    print(sum_of_diff)
-    print(wordcount)
-    print(sum_of_diff / wordcount)
+
+# def find_avg_diff_of_folder():
+#     sum_of_diff = 0
+#     wordcount = 0
+#     for file in os.listdir(os.getcwd()):
+#         file_path = (os.path.join(os.getcwd(), file))
+#         print(file_path)
+#         subtitles = parser.parse(file_path)
+#         print(subtitles)
+#
+#
+#         for subtitle in subtitles:
+#             print(subtitle.text)
+#             parsed_line = wakati.parse(subtitle.text).split()
+#             print(parsed_line)
+#             for word in parsed_line:
+#                 print(word)
+#                 for entry in freq_dic.freq_list:
+#                     if word == entry[0]:
+#                         wordcount += 1
+#                         sum_of_diff += entry[2]
+#                         print(entry[2])
+#     print(sum_of_diff)
+#     print(wordcount)
+#     print(sum_of_diff / wordcount)
 
 
 
