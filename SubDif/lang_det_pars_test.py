4773bc993124dd9b8a2bc06507ae13f776aa5c9e
tvvoty <75815245+tvvoty@users.noreply.github.com>
2021-05-12 03:37:10 +0300

    Maybe starting working on it again

---
+++ SubDif/lang_det_pars_test.py
@@ -0,0 +1,153 @@
+# main file with all the functions
+
+from pysubparser import parser  # parses subtitle files, now that i think about it, it's prolly unnecesary since
+# mecab parses every thing anyway
+import freq_dic_hashtable  # A hash table with difficulty of words, created from
+import MeCab  # Parses japanese text
+import os
+import glob
+from langdetect import detect
+from langdetect import detect_langs
+
+# finds avg_diff of every title in a folder with nested filders
+def find_avg_diff_of_folder_with_hashtable():
+    for folder in os.listdir(main_folder):
+        print(folder)
+        sub_folder_path = f'{main_folder}\\{folder}'
+        os.chdir(sub_folder_path)
+        print(sub_folder_path)
+        sum_of_diff = 0
+        wordcount = 0
+        allfiles = glob.glob(f"{os.getcwd()}\\*.*", recursive=True)
+        print(f'All file in glob globe func are')
+        for x in allfiles:
+            mes = x
+            print(x)
+
+        # for file in os.listdir(os.getcwd()):
+        for file in glob.glob(f"{os.getcwd()}\\**\\**.**", recursive=True):
+            file_path = (os.path.join(os.getcwd(), file))
+            print(file_path)
+            try:
+
+                # langdetect part
+                line_for_langdet = ""
+                subtitles = parser.parse(file_path)
+                for subtitle in subtitles:
+                    line_for_langdet += subtitle.text
+                print(line_for_langdet)
+                lang_det_det = detect(line_for_langdet)
+                print(f"The language is {lang_det_det}")
+
+                langs_det_det = detect_langs(line_for_langdet)
+                print(f"The lang probabilities are: {langs_det_det}")
+                # print(subtitles)
+
+                for subtitle in subtitles:
+                    # print(subtitle.text)
+                    parsed_line = wakati.parse(subtitle.text).split()
+                    # print(parsed_line)
+                    for word in parsed_line:
+                        # print(word)
+                        if word in freq_dic_hashtable.freq_hashtable:
+                            wordcount += 1
+                            sum_of_diff += freq_dic_hashtable.freq_hashtable[word]
+                            # print(freq_dic_hashtable.freq_hashtable[word])
+            except:
+                print("Wrong file format.")
+        # print(sum_of_diff)
+        # print(wordcount)
+        try:
+            print(sum_of_diff / wordcount)
+            f.write(f'"{folder}" : [{sum_of_diff},{wordcount},{sum_of_diff / wordcount}]' + ", \n")
+        except:
+            print("Error, prolly division by zero, prolly not important.")
+            f.write(f'"{folder}" : [{sum_of_diff},{wordcount},\"Error in av_dif func in line 47\"]' + ", \n")
+
+
+
+
+
+# parse subtitles with MeCab
+wakati = MeCab.Tagger("-Owakati")
+
+main_folder = ("F:\\GitHub\\AvJDif\\SubDif\\sub_archive_simulation")
+# main_folder = ("F:\Afordearch")
+with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.py", mode='w', encoding='utf-8') as f:
+    f.write('anime_avg_diff_table = {')
+
+    find_avg_diff_of_folder_with_hashtable()
+
+with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.py", 'rb+') as f:
+    f.seek(-4, os.SEEK_END)
+    f.truncate()
+
+with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.py", mode='a', encoding='utf-8') as f:
+    f.write('}')
+
+
+
+
+
+
+
+
+# def find_avg_diff_of_folder():
+#     sum_of_diff = 0
+#     wordcount = 0
+#     for file in os.listdir(os.getcwd()):
+#         file_path = (os.path.join(os.getcwd(), file))
+#         print(file_path)
+#         subtitles = parser.parse(file_path)
+#         print(subtitles)
+#
+#
+#         for subtitle in subtitles:
+#             print(subtitle.text)
+#             parsed_line = wakati.parse(subtitle.text).split()
+#             print(parsed_line)
+#             for word in parsed_line:
+#                 print(word)
+#                 for entry in freq_dic.freq_list:
+#                     if word == entry[0]:
+#                         wordcount += 1
+#                         sum_of_diff += entry[2]
+#                         print(entry[2])
+#     print(sum_of_diff)
+#     print(wordcount)
+#     print(sum_of_diff / wordcount)
+
+
+
+
+# def dum():
+#     # test
+#     # for subtitle in subtitles:
+#     #     print(subtitle.text)
+#     pass
+
+
+
+
+
+
+
+
+# old function for one file
+# def find_avg_diff():
+#     sum_of_diff = 0
+#     wordcount = 0
+#     for subtitle in subtitles:
+#         print(subtitle.text)
+#         parsed_line = wakati.parse(subtitle.text).split()
+#         print(parsed_line)
+#         for word in parsed_line:
+#             print(word)
+#             for entry in freq_dic.freq_list:
+#                 if word == entry[0]:
+#                     wordcount += 1
+#                     sum_of_diff += entry[2]
+#                     print(entry[2])
+#     print(sum_of_diff)
+#     print(wordcount)
+#     print(sum_of_diff / wordcount)
---
+++ SubDif/to_parser_test.py
@@ -0,0 +1,157 @@
+# main file with all the functions
+
+from pysubparser import parser  # parses subtitle files, now that i think about it, it's prolly unnecesary since
+# mecab parses every thing anyway
+import freq_dic_hashtable  # A hash table with difficulty of words, created from
+import MeCab  # Parses japanese text
+import os
+import glob
+from langdetect import detect
+from langdetect import detect_langs
+
+
+def isJapanese(text):
+    lang_det_det = detect(text)
+    print(f"The language is {lang_det_det}")
+
+    langs_det_det = detect_langs(text)
+    print(f"The lang probabilities are: {langs_det_det}")
+    if lang_det_det == "ja":
+        return True
+    if lang_det_det == "en":
+        return True
+    return False
+
+
+# finds avg_diff of every title in a folder with nested filders
+def find_avg_diff_of_folder_with_hashtable():
+    for folder in os.listdir(main_folder):
+        print(folder)
+        sub_folder_path = f'{main_folder}\\{folder}'
+        os.chdir(sub_folder_path)
+        print(sub_folder_path)
+        sum_of_diff = 0
+        wordcount = 0
+        allfiles = glob.glob(f"{os.getcwd()}\\*.*", recursive=True)
+        print(f'All file in glob globe func are')
+        for x in allfiles:
+            mes = x
+            print(x)
+
+        # for file in os.listdir(os.getcwd()):
+        for file in glob.glob(f"{os.getcwd()}\\**\\**.**", recursive=True):
+            file_path = (os.path.join(os.getcwd(), file))
+            print(file_path)
+            try:
+                with open(file_path, mode='r', encoding='utf-8') as sub_file:
+                    sub_file_content = sub_file.read()
+                    if isJapanese(sub_file_content):
+                        parsed_text = wakati.parse(sub_file_content).split()
+                        # print(parsed_text)
+                        for word in parsed_text:
+                            # print(word)
+                            if word in freq_dic_hashtable.freq_hashtable:
+                                wordcount += 1
+                                sum_of_diff += freq_dic_hashtable.freq_hashtable[word]
+                                # print(freq_dic_hashtable.freq_hashtable[word])
+                    else:
+                        print("The file is not jp sub")
+            except Exception as e:
+                print(e)
+                # print("Wrong file format.")
+        # print(sum_of_diff)
+        # print(wordcount)
+        try:
+            print(sum_of_diff / wordcount)
+            f.write(f'"{folder}" : [{sum_of_diff},{wordcount},{sum_of_diff / wordcount}]' + ", \n")
+        except:
+            print("Error, prolly division by zero, prolly not important.")
+            f.write(f'"{folder}" : [{sum_of_diff},{wordcount},\"Error in av_dif func in line 47\"]' + ", \n")
+
+
+
+
+
+# parse subtitles with MeCab
+wakati = MeCab.Tagger("-Owakati")
+
+main_folder = ("F:\\GitHub\\AvJDif\\SubDif\\sub_archive_simulation")
+# main_folder = ("F:\Afordearch")
+with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.py", mode='w', encoding='utf-8') as f:
+    f.write('anime_avg_diff_table = {')
+
+    find_avg_diff_of_folder_with_hashtable()
+
+with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.py", 'rb+') as f:
+    f.seek(-4, os.SEEK_END)
+    f.truncate()
+
+with open("F:\\GitHub\\AvJDif\\SubDif\\stats_log.py", mode='a', encoding='utf-8') as f:
+    f.write('}')
+
+
+
+
+
+
+
+
+# def find_avg_diff_of_folder():
+#     sum_of_diff = 0
+#     wordcount = 0
+#     for file in os.listdir(os.getcwd()):
+#         file_path = (os.path.join(os.getcwd(), file))
+#         print(file_path)
+#         subtitles = parser.parse(file_path)
+#         print(subtitles)
+#
+#
+#         for subtitle in subtitles:
+#             print(subtitle.text)
+#             parsed_line = wakati.parse(subtitle.text).split()
+#             print(parsed_line)
+#             for word in parsed_line:
+#                 print(word)
+#                 for entry in freq_dic.freq_list:
+#                     if word == entry[0]:
+#                         wordcount += 1
+#                         sum_of_diff += entry[2]
+#                         print(entry[2])
+#     print(sum_of_diff)
+#     print(wordcount)
+#     print(sum_of_diff / wordcount)
+
+
+
+
+# def dum():
+#     # test
+#     # for subtitle in subtitles:
+#     #     print(subtitle.text)
+#     pass
+
+
+
+
+
+
+
+
+# old function for one file
+# def find_avg_diff():
+#     sum_of_diff = 0
+#     wordcount = 0
+#     for subtitle in subtitles:
+#         print(subtitle.text)
+#         parsed_line = wakati.parse(subtitle.text).split()
+#         print(parsed_line)
+#         for word in parsed_line:
+#             print(word)
+#             for entry in freq_dic.freq_list:
+#                 if word == entry[0]:
+#                     wordcount += 1
+#                     sum_of_diff += entry[2]
+#                     print(entry[2])
+#     print(sum_of_diff)
+#     print(wordcount)
+#     print(sum_of_diff / wordcount)
